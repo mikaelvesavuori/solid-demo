a042c049f15ca57bd225901f6d7361a9b8469645
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2023-10-18 14:54:09 +0200

    Update

--- src/start.ts
+++ src/start.ts
@@ -7,19 +7,15 @@
 
 // An example of your everyday, bog-standard one-file "dirty" implementation.
 
-function main() {
-  const address = 'sam.person@company.xyz';
-  const [name, domain] = address.split('@');
-  const isValid = name && domain && name.length > 2 && domain.length > 2;
+const addr = 'sam.person@company.xyz';
+const [n, d] = addr.split('@');
+const ok = n && d && n.length > 2 && d.length > 2;
 
-  if (isValid) {
-    const content = `Hello! Thanks for getting in touch. A customer sales agent will respond within 24 hours. Have a nice day!`;
-    // Implement sending an email...
-    // emailLibrary.sendEmail(addr, content);
-    console.log(`Sent email to ${address}: ${content}`);
-  } else {
-    throw new Error('Bad address');
-  }
+if (ok) {
+  const content = `Hello! Thanks for getting in touch. A customer sales agent will respond within 24 hours. Have a nice day!`;
+  // Implement sending an email...
+  // emailLibrary.sendEmail(addr, content);
+  console.log(`Sent email to ${addr}: ${content}`);
+} else {
+  throw new Error('Bad address');
 }
-
-main();
